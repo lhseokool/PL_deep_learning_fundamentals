20b4d2e3aa711a70dc51aeead52ed46a7217c762
lhseokool <45442353+lhseokool@users.noreply.github.com>
2024-01-14 04:04:24 +0000

    RNN update

--- 08_rnn/myRNN.py
+++ 08_rnn/myRNN.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class MyRNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
@@ -9,8 +10,9 @@ class MyRNN(nn.Module):
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
         self.h2o = nn.Linear(hidden_size, output_size)
 
+    
     def forward(self, input, hidden):
-        combined = torch.cat((input,hidden), 1)
+        combined = torch.cat((input, hidden), 1)
         hidden = torch.tanh(self.i2h(combined))
         output = self.h2o(hidden)
         return output, hidden
@@ -20,5 +22,6 @@ class MyRNN(nn.Module):
     
 
 
-rnn_model = MyRNN(input_size=4, hidden_size=1024, output_size=2)
+rnn_model = MyRNN(input_size=5, hidden_size=1024, output_size=2)
 hidden = rnn_model.get_hidden()
+out, hidden = rnn_model(torch.tensor([[1,2,3,4,5]]), hidden)
